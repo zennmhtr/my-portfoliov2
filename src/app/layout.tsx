3a73c00720bab04576c54e47243c1fa731e95adb
Zaini Muhtarom <120488217+zennmhtr@users.noreply.github.com>
2026-08-05 15:02:01 +0700

    My Portfolio V2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,13 +7,13 @@ import "./globals.css";
 const inter = Space_Grotesk({ subsets: ["latin"], weight: ["500", "600", "700"] });
 
 export const metadata: Metadata = {
-  title: "Zaini Muhtarom - Web Developer",
+  title: "Zaini Muhtarom - Fullstack Web Developer",
   description: " Passionate about building scalable web applications, optimizing system performance, and delivering user-centric digital solutions with intuitive interfaces.",
-  keywords: "Zaini Muhtarom, Full Web Developer, Laravel, PHP, TypeScript, Web Development, Portfolio",
+  keywords: "Zaini Muhtarom, Fullstack Web Developer, React.js, Javascript, Typescript , Laravel, PHP, TypeScript, Web Development, Portfolio",
   authors: [{ name: "Zaini Muhtarom" }],
   openGraph: {
     title: "Zaini Muhtarom - Web Developer",
-    description: "Portfolio of Zaini Muhtarom, a passionate full-web developer specializing in modern web technologies.",
+    description: "Portfolio of Zaini Muhtarom, a passionate fullstack-web developer specializing in modern web technologies.",
     type: "website",
   },
 };
